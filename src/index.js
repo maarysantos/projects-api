a3aa190f67ef8808e8cae82689e75d421cd48b4a
MaarySantos <maary.santos1@gmail.com>
2019-04-12 19:45:07 -0300

    compressed and protecting requisitions

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@ const bodyParser = require('body-parser');
 const cors = require ('cors');
 const morgan = require('morgan');
 const logger = require ('../src/modules/logger');
+const compression = require ('compression');
+const helmet = require('helmet');
+
 
 const app = express();
 
-//module.exports = app =>{
-   // app.set('port', 300);
-   // app.set('json spaces',4);
-    app.use(morgan('common',{
-        stream :{
-            write :(message) =>{
-                logger.info(message);
-            }
+app.use(morgan('common',{
+    stream :{
+        write :(message) =>{
+            logger.info(message);
         }
     }
-    ))
-//};
-
+}));
+app.use(helmet());
 app.use(cors({
     origin :['http://localhost:3001'],
     methods : ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders : ['Content_Type', 'Authorization']
 }));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use ((req, res, next)=>{
